cc77276086ddef3f69f356c716725fe6723b8d97
Zurich Sabil <zurichsabil@gmail.com>
2025-08-31 12:49:49 +0700

    Add files via upload

---
+++ sabil-project1/src/main/java/com/mycompany/sabil/project1/SabilProject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sabil.project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SabilProject1 {
+    public static void main(String[] args) {
+        String name = "Zurich Sabil";
+        String npm = "2430506057";
+        String alamat = "Jalan Kusuma Bangsa, Magelang Kota Magelang";
+        String notp = "081234567890"; // Diperbaiki dari 'string' menjadi 'String'
+
+        // Menampilkan data ke layar
+        System.out.println("Nama: " + name);
+        System.out.println("NPM: " + npm);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("No. Telepon: " + notp);
+    }
+}
