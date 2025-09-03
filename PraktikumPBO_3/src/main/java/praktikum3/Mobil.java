a17be638cf290300ad3ca2a2e106c7e0451182d4
Zurich Sabil <zurichsabil@gmail.com>
2025-09-03 16:04:43 +0700

    Add files via upload

--- PraktikumPBO_3/src/main/java/praktikum3/Mobil.java
+++ PraktikumPBO_3/src/main/java/praktikum3/Mobil.java
@@ -1,49 +1,50 @@
 package praktikum3;
 
 public class Mobil {
-    String merk;
-    String model;
-    int tahun;
-    String Warna;
+    private String merk;
+    private String model;
+    private int tahun;
+    private String warna;
 
-public Mobil(String merk, String model, int tahun) {
+    public Mobil(String merk, String model, int tahun, String warna) {
         this.merk = merk;
         this.model = model;
         this.tahun = tahun;
-}
-public String getMerk() {
-    return merk;
-}
-
-public void setMerk(String merk) {
-    this.merk = merk;
-}
-
-public String getModel() {
-    return model;
-}
-
-public void setModel(String model) {
-    this.model = model;
-}
-
-public int getTahun() {
-    return tahun;
-}
-
-public void setTahun(int tahun) {
-    this.tahun = tahun;
-}
-
-public void displayInfo() {
+        this.warna = warna;
+    }
+    public String getMerk() {
+        return merk;
+    }
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getTahun() {
+        return tahun;
+    }
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+    public String getWarna() {
+        return warna;
+    }
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    public void displayInfo() {
     System.out.println("Merk mobil tersebut adalah " + merk + " dengan Model " + model + 
-                       " dan keluaran tahun " + tahun);
+                       " dan keluaran tahun " + tahun + " Berwarna " + warna);
 }
-public void startEngine() {
-    System.out.println("Mesin mobil " + merk + " menyala");
-}
-public void ubahWarna(String warnaBaru) {
-    this.Warna = warnaBaru;
+     public void startEngine() {
+        System.out.println("Mesin mobil " + merk + " menyala");
+    }
+   public void ubahWarna(String warnaBaru) {
+    this.warna = warnaBaru;
     System.out.println("Warna mobil diubah menjadi " + warnaBaru);
 }
 }
--- PraktikumPBO_3/src/main/java/praktikum3/main2.java
+++ PraktikumPBO_3/src/main/java/praktikum3/main2.java
@@ -2,10 +2,10 @@ package praktikum3;
 
 public class main2 {
     public static void main(String[] args) {
-        Mobil mobil1 = new Mobil("Mercedes", "Suzuki", 2020);
-        Mobil mobil2 = new Mobil("Honda", "Wuling", 2021);
+        Mobil mobil1 = new Mobil("Toyota", "BmW", 2020, "hitam");
+        Mobil mobil2 = new Mobil("Honda", "Civic", 2021, "putih");
 
-       mobil1.displayInfo();
+        mobil1.displayInfo();
         mobil1.startEngine();
         mobil1.ubahWarna("Merah");
         mobil1.displayInfo();
